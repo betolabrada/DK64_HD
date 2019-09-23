c09a94521ccd7c40f99031aaabf89004564e0484
Gustavo Flores <37113826+Gussiny@users.noreply.github.com>
2019-09-23 12:37:45 -0500

    Add nepe

---
+++ Assets/Scripts/PlayerJump.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerJump : MonoBehaviour {
+
+    public float speed = 10f;
+
+    Rigidbody playerRigidbody;
+    public bool estaEnPiso;
+
+    void Start()
+    {
+        estaEnPiso = true;
+        playerRigidbody = GetComponent<Rigidbody>();
+    }
+
+    void FixedUpdate()
+    {
+
+        if (Input.GetButtonDown("Jump") && estaEnPiso)
+        {
+            Jump();
+            estaEnPiso = false;
+        }
+    }
+
+    void Jump()
+    {
+        playerRigidbody.AddForce(new Vector3(0,10,0), ForceMode.Impulse);
+    }
+
+    void OnCollisionEnter(Collision c)
+    {
+        if (c.gameObject.tag == "Ground" && !estaEnPiso)
+        {
+            print("en piso");
+            estaEnPiso = true;
+        }
+    }
+
+}
