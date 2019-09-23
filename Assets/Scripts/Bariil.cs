94d70d59fc713a51a41624897aeca1a82e586582
obravocedillo <oliverbravo45@gmail.com>
2019-09-23 12:38:34 -0500

    Add barril mierda

---
+++ Assets/Scripts/Bariil.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bariil : MonoBehaviour {
+    private Rigidbody rb;
+    public float radius = 5.0F;
+    public float power = 10.0F;
+    // Use this for initialization
+    void Start () {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.layer == 11){
+            rb.AddExplosionForce(power, transform.position, radius, 3.0F, ForceMode.Impulse);
+            //collision.gameObject.GetComponent<Rigidbody>().AddExplosionForce(10, Vector3.zero, 10);
+        } else if (collision.gameObject.layer == 12){
+            rb.AddExplosionForce(power, transform.position, radius, 3.0F, ForceMode.Impulse);
+            //collision.gameObject.GetComponent<Rigidbody>().AddExplosionForce(10, Vector3.zero, 10);
+        }
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
