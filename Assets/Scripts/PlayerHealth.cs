16ccd9dd95ccef59b5b17fd43197414478e5cf48
Alberto Labrada <31485362+betolabrada@users.noreply.github.com>
2019-09-23 11:23:31 -0500

    scripts de player

---
+++ Assets/Scripts/PlayerHealth.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerHealth : MonoBehaviour {
+
+    public int saludInicial = 10;
+    public int saludActual;
+
+    PlayerMov playerMov;
+    bool danio;
+    bool estaMuerto;
+    
+    void Start()
+    {
+        saludActual = saludInicial;
+    }
+
+    void Update()
+    {
+        if (danio)
+        {
+            // script de sandias
+        }
+        danio = false;
+    }
+    public void HacerDanio(int cantidad)
+    {
+        danio = true;
+        saludActual -= cantidad;
+
+        if (saludActual <= 0 && !estaMuerto)
+        {
+            Muerte();
+        }
+    }
+
+    void Muerte()
+    {
+        estaMuerto = true;
+
+        playerMov.enabled = false; 
+    }
+}
---
+++ Assets/Scripts/PlayerMov.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMov : MonoBehaviour {
+
+    public float speed = 5f;
+
+    Rigidbody playerRigidbody;
+    Vector3 movement;
+
+    // Use this for initialization
+    void Start () {
+        playerRigidbody = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float h = Input.GetAxisRaw("Horizontal");
+        float v = Input.GetAxisRaw("Vertical");
+
+        Move(h, v);
+	}
+
+    void Move(float h, float v)
+    {
+        movement.Set(h, 0f, v);
+
+        // normalize
+        movement = movement.normalized * speed * Time.deltaTime;
+
+        playerRigidbody.MovePosition(transform.position + movement);
+    }
+}
