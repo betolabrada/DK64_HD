d2fab2b4173b071672e62fb1cd6f84aed597245a
obravocedillo <oliverbravo45@gmail.com>
2019-11-06 09:13:09 -0600

    nivel uno

---
+++ Assets/Scripts/Teleporter.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Teleporter : MonoBehaviour {
+    public GameObject hermano;
+    private Rigidbody rb;
+
+    // Use this for initialization
+    void Start(){
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void OnCollisionEnter(Collision col){
+        if (col.gameObject.tag == "Players")
+        {
+            col.gameObject.transform.position = hermano.transform.position;
+        }
+    }
+
+
+
+    // Update is called once per frame
+    void Update ()
+    {
+		
+    }
+}
---
+++ Assets/Scripts/WeaponCd.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponCd : MonoBehaviour {
+    private float timeStamp;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        // 10 seg de cooldown del arma chida
+        timeStamp = Time.time + 10;
+        if (timeStamp <= Time.time){
+            //clonar objeto
+        }
+    }
+}
