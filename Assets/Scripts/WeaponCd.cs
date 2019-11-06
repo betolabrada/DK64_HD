4d480bb3d92cf9d39e935eed9cbda28edc32b5d6
obravocedillo <oliverbravo45@gmail.com>
2019-11-06 10:58:39 -0600

    arma chida con sonido

--- Assets/Scripts/WeaponCd.cs
+++ Assets/Scripts/WeaponCd.cs
@@ -3,19 +3,40 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class WeaponCd : MonoBehaviour {
+    private Rigidbody rb;
     private float timeStamp;
-
+    private bool active;
+    AudioSource playerAudio;
     // Use this for initialization
-    void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-        // 10 seg de cooldown del arma chida
-        timeStamp = Time.time + 10;
-        if (timeStamp <= Time.time){
-            //clonar objeto
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        playerAudio = GetComponent<AudioSource>();
+        active = true;
+    }
+
+    void OnCollisionEnter(Collision col)
+    {
+        if (col.gameObject.tag == "Player")
+        {
+            if (timeStamp <= Time.time)
+            {
+                //  activar arma chida en jugador
+                gameObject.GetComponent<Renderer>().enabled = false;
+                playerAudio.Play();
+                active = false;
+                timeStamp = Time.time + 8;
+            }
+
+        }
+    }
+
+
+    // Update is called once per frame
+    void Update () {
+        if (timeStamp <= Time.time)
+        {
+            gameObject.GetComponent<Renderer>().enabled = true;
         }
     }
 }
