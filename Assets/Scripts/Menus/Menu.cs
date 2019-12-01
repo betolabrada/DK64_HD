896d9d2a96b32d8bdbf3035b6b1c0927d50b299d
Gustavo Flores <37113826+Gussiny@users.noreply.github.com>
2019-12-01 08:57:41 -0600

    IntroVideo

--- Assets/Scripts/Menus/Menu.cs
+++ Assets/Scripts/Menus/Menu.cs
@@ -2,10 +2,38 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
 
 public class Menu : MonoBehaviour {
+	public GameObject modos ,este;
+	public GameObject grupo1, grupo2;
+	public GameObject nextBtn;
+	private int grupoActual;
+	void Start(){
+		grupoActual = 1;
+	}
 	public void CambiarEscena(){
-		SceneManager.LoadScene("FirstMap");
+		SceneManager.LoadScene("SelectMode");
+		
+	}
+
+	public void CambiarModo(){
+		modos.SetActive(true);
+		este.SetActive(false);
+	}
+	
+	public void NextGroup(){
+		grupo1.SetActive(false);
+		grupo2.SetActive(true);
+		this.gameObject.SetActive(false);
+		
+	}
+
+	public void BackGroup(){
+		grupo2.SetActive(false);
+		grupo1.SetActive(true);
+		nextBtn.SetActive(true);
 	}
 
 	public void ModoDummy(){
---
+++ Assets/Scripts/Menus/SkipVideo.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class SkipVideo : MonoBehaviour {
+
+	void FixedUpdate () {
+		if(Input.GetKeyDown("joystick 1 button 0") | Input.GetKeyDown(KeyCode.Return) | Input.GetKeyDown(KeyCode.Space)){
+			SceneManager.LoadScene("MenuPrincipal");
+		}
+	}
+}
