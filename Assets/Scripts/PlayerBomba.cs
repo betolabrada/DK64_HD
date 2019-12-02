b7946f082e0c588f5c53762d87f4fe350abb7fa2
Gustavo Flores <37113826+Gussiny@users.noreply.github.com>
2019-12-02 00:57:43 -0600

    Animacion Bomba

--- Assets/Scripts/PlayerBomba.cs
+++ Assets/Scripts/PlayerBomba.cs
@@ -9,19 +9,22 @@ public class PlayerBomba : MonoBehaviour {
     public GameObject gameManagerObject;
 
     GameManager gameManager;
+
+    Animator animator;
     int playerN;
 
     void Awake ()
     {
         gameManager = gameManagerObject.GetComponent<GameManager>();
         playerN = GetComponent<PlayerMov>().playerN;
-
+        animator = GetComponent<PlayerMov>().animator;
     }
 
     void Update ()
     {
         if ((Input.GetButtonDown("P" + playerN + "B") && gameManager.modoActual != 8 && gameManager.modoActual != 7))
         {
+            animator.SetTrigger("Punch");
             GameObject inst = Instantiate(bomba, bombRef.position, bombRef.rotation);
             Physics.IgnoreCollision(inst.GetComponent<Collider>(), GetComponent<Collider>());
             
